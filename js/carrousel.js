3d1edfc456a451dddd102dc341980e336412d38a
raythestudent-maisonneuve <70280810+Raythestudent-momo@users.noreply.github.com>
2024-04-18 16:02:52 -0400

    s10c2 - début carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,29 @@
 (function(){
-    console.log('début du carrousel');
-})
+    console.log('début du carrousel')
+    let carrousel = document.querySelector('.carrousel')
+    let bouton = document.querySelector('.bouton__ouvrir')
+    let carrousel__x = document.querySelector('.carrousel__x')
+
+    let galerie = document.querySelector('.galerie')
+    let galerie__img = galerie.querySelectorAll('img')
+    let carrousel__figure = document.querySelector('.carrousel__figure')
+
+    for (const elm of galerie__img) {
+        let carrousel__img = document.createElement('img')
+        carrousel__img.classList.add('carrousel__img')
+        console.log(elm.src)
+        carrousel__img.src = elm.src
+        console.log(carrousel__img.src)
+        carrousel__figure.appendChild(carrousel__img)
+    }
+
+
+
+    bouton.addEventListener('mousedown', function(){
+        carrousel.classList.add('carrousel--ouvrir')
+    })
+
+    carrousel__x.addEventListener('mousedown', function(){
+        carrousel.classList.remove('carrousel--ouvrir')
+    })
+})()
