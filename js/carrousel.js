9f83ddd1650ce2c3179e3136201dac0043ba4417
raythestudent-maisonneuve <70280810+Raythestudent-momo@users.noreply.github.com>
2024-04-18 16:42:56 -0400

    s10c2 - tentative 1 de créer un carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -15,6 +15,26 @@
         carrousel__img.src = elm.src
         console.log(carrousel__img.src)
         carrousel__figure.appendChild(carrousel__img)
+
+        let carrousel__form = document.querySelector('.carrousel__form')
+        let carrousel__input = document.createElement('input')
+        carrousel__input.classList.add('carrousel__input')
+        carrousel__input.type = 'radio'
+        carrousel__input.name = 'carrousel'
+        carrousel__input.id = elm.id
+        carrousel__input.value = elm.id
+        carrousel__form.appendChild(carrousel__input)
+
+        carrousel__input.addEventListener('change', function(){
+            let id = this.value
+            console.log(id)
+            let img = carrousel__figure.querySelector(`${id}`)
+            let carrousel__img = carrousel__figure.querySelectorAll('.carrousel__img')
+            for (const elm of carrousel__img) {
+                elm.classList.remove('carrousel__img--visible')
+            }
+            img.classList.add('carrousel__img--visible')
+        })
     }
 
 
