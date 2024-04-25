082bcf5f0d57198a524c2386688bec606c8e14e9
raythestudent-maisonneuve <70280810+Raythestudent-momo@users.noreply.github.com>
2024-04-25 15:32:50 -0400

    s11c2 - transition des images du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -3,18 +3,18 @@
     let carrousel = document.querySelector('.carrousel')
     let bouton = document.querySelector('.bouton__ouvrir')
     let carrousel__x = document.querySelector('.carrousel__x')
-    
+
     let galerie = document.querySelector('.galerie')
     let galerie__img = galerie.querySelectorAll('img')
     let carrousel__figure = document.querySelector('.carrousel__figure')
-    
+
     for (const [index, elm] of Array.from(galerie__img).entries()) {
         let carrousel__img = document.createElement('img')
         carrousel__img.classList.add('carrousel__img')
-        carrousel__img.id = 'img-' + index; // set id for the image
+        carrousel__img.id = 'img-' + index;
         carrousel__img.src = elm.src
         carrousel__figure.appendChild(carrousel__img)
-    
+
         let carrousel__form = document.querySelector('.carrousel__form')
         let carrousel__input = document.createElement('input')
         carrousel__input.classList.add('carrousel__input')
@@ -23,10 +23,15 @@
         carrousel__input.id = 'radio-' + index
         carrousel__input.value = index
         carrousel__form.appendChild(carrousel__input)
-    
+        carrousel__input.checked = index === 0
+
+        if(index === 0){
+            carrousel__img.classList.add('carrousel__img--visible')
+        }
+
         carrousel__input.addEventListener('change', function(){
             let id = this.value
-            let img = carrousel__figure.querySelector(`#img-${id}`) // select the image using the correct id
+            let img = carrousel__figure.querySelector(`#img-${id}`)
             let carrousel__img = carrousel__figure.querySelectorAll('.carrousel__img')
             for (const elm of carrousel__img) {
                 elm.classList.remove('carrousel__img--visible')
@@ -34,11 +39,11 @@
             img.classList.add('carrousel__img--visible')
         })
     }
-    
+
     bouton.addEventListener('mousedown', function(){
         carrousel.classList.add('carrousel--ouvrir')
     })
-    
+
     carrousel__x.addEventListener('mousedown', function(){
         carrousel.classList.remove('carrousel--ouvrir')
     })
