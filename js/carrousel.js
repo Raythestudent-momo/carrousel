09390bac31f158bc12e0b13ed5c067e3ede1e643
raythestudent-maisonneuve <70280810+Raythestudent-momo@users.noreply.github.com>
2024-04-25 13:49:31 -0400

    s11c2 - fonctionnalité pour changer les images
    programmation des boutons radio pour qu'ils affichent les images sur un "change"

--- js/carrousel.js
+++ js/carrousel.js
@@ -3,32 +3,30 @@
     let carrousel = document.querySelector('.carrousel')
     let bouton = document.querySelector('.bouton__ouvrir')
     let carrousel__x = document.querySelector('.carrousel__x')
-
+    
     let galerie = document.querySelector('.galerie')
     let galerie__img = galerie.querySelectorAll('img')
     let carrousel__figure = document.querySelector('.carrousel__figure')
-
-    for (const elm of galerie__img) {
+    
+    for (const [index, elm] of Array.from(galerie__img).entries()) {
         let carrousel__img = document.createElement('img')
         carrousel__img.classList.add('carrousel__img')
-        console.log(elm.src)
+        carrousel__img.id = 'img-' + index; // set id for the image
         carrousel__img.src = elm.src
-        console.log(carrousel__img.src)
         carrousel__figure.appendChild(carrousel__img)
-
+    
         let carrousel__form = document.querySelector('.carrousel__form')
         let carrousel__input = document.createElement('input')
         carrousel__input.classList.add('carrousel__input')
         carrousel__input.type = 'radio'
         carrousel__input.name = 'carrousel'
-        carrousel__input.id = elm.id
-        carrousel__input.value = elm.id
+        carrousel__input.id = 'radio-' + index
+        carrousel__input.value = index
         carrousel__form.appendChild(carrousel__input)
-
+    
         carrousel__input.addEventListener('change', function(){
             let id = this.value
-            console.log(id)
-            let img = carrousel__figure.querySelector(`${id}`)
+            let img = carrousel__figure.querySelector(`#img-${id}`) // select the image using the correct id
             let carrousel__img = carrousel__figure.querySelectorAll('.carrousel__img')
             for (const elm of carrousel__img) {
                 elm.classList.remove('carrousel__img--visible')
@@ -36,13 +34,11 @@
             img.classList.add('carrousel__img--visible')
         })
     }
-
-
-
+    
     bouton.addEventListener('mousedown', function(){
         carrousel.classList.add('carrousel--ouvrir')
     })
-
+    
     carrousel__x.addEventListener('mousedown', function(){
         carrousel.classList.remove('carrousel--ouvrir')
     })
